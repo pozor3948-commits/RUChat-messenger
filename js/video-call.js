7f0f0812fda6bd82ae99f7a09e2c3b30818a0489
pozor3948-commits <pozor3948@gmail.com>
2026-02-23 13:29:33 +0300

    звонки

--- js/video-call.js
+++ js/video-call.js
@@ -17,8 +17,29 @@ let incomingVideoCallData = null;
 // Конфигурация WebRTC для видеозвонков
 const videoRtcConfiguration = {
     iceServers: [
+        // STUN серверы Google (бесплатные)
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:stun1.l.google.com:19302' }
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
+        
+        // Бесплатные TURN серверы (OpenRelay)
+        {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
     ]
 };
 
